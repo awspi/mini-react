6f1b2e2d5bcca6a519aabd8b05a71721ecc7d694
awspi <206002328@qq.com>
2023-01-25 07:21:41 +0800

    feat: 实现hooks架构、useState

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,5 +1,19 @@
+import currentDispatcher, {
+	Dispatcher,
+	resolveDispatcher
+} from './src/currentDispatcher'
 import { jsxDEV } from './src/jsx'
 export default {
 	version: '0.0.0',
 	createElement: jsxDEV
+}
+
+export const useState: Dispatcher['useState'] = (initialState) => {
+	const dispatcher = resolveDispatcher()
+	return dispatcher.useState(initialState)
+}
+
+//内部数据共享层  对应原版的__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+export const __SECRET_INTERNALS = {
+	currentDispatcher
 }
--- scripts/rollup/react-dom.config.js
+++ scripts/rollup/react-dom.config.js
@@ -1,7 +1,7 @@
 import { getBaseRollupPlugins, getPackageJSON, resolvePkgPath } from './utils'
 import generatePackageJson from 'rollup-plugin-generate-package-json'
 import alias from '@rollup/plugin-alias'
-const { name, module } = getPackageJSON('react-dom')
+const { name, module, peerDependencies } = getPackageJSON('react-dom')
 //react-dom 包的路径
 const pkgPath = resolvePkgPath(name)
 //react-dom 产物路径
@@ -24,6 +24,8 @@ export default [
 				format: 'umd' //兼容cmj esm
 			}
 		],
+		//不打包到react-dom
+		external: [...Object.keys(peerDependencies)],
 		plugins: [
 			...getBaseRollupPlugins(),
 			//类似 webpack resolve alias的功能
